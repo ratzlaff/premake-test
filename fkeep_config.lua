6e8960ac3e87dbad860a081ad553f501918214d7
Ben Ratzlaff <ben.ratzlaff@gmail.com>
2019-03-05 14:34:40 -0700

    Set objdir and targetdir

--- fkeep_config.lua
+++ fkeep_config.lua
@@ -11,6 +11,8 @@ end
 function fkeep.defaultProject(inName)
 	inName = inName or "main"
 	project(inName)
+		objdir "tmp/obj/%{cfg.name}"
+		targetdir "tmp/bin/%{cfg.name}"
 		location "log"
 		kind "ConsoleApp"
 		files "main.cpp"
